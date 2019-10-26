819b11dc34149d2f025b18d1a0aa228a4bf600ad
Aousnik Gupta <43369060+gittygupta@users.noreply.github.com>
2019-10-26 20:02:10 +0530

    Delete prog6.py

--- Day 9/prog6.py
+++
@@ -1,8 +0,0 @@
-import calendar
-x = list(raw_input('Enter Range:').split())
-tot = 0
-print('Leap Years in range:')
-for i in range(int(x[0]), int(x[1])):
-    if calendar.isleap(i):
-        tot += 1
-print(tot)
